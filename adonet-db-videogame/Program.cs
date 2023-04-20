9029363b3a89f47a7171f7c32a527897fe5c2cf1
AlessandroDiNardo <alexdna003@gmail.com>
2023-04-20 16:08:44 +0200

    add options, have to write the cases

--- adonet-db-videogame/Program.cs
+++ adonet-db-videogame/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Chiedo all'utente cosa vuole fare
+            Console.WriteLine("1. Inserisci un nuovo videogioco");
+            Console.WriteLine("2. Ricerca videogioco per ID");
+            Console.WriteLine("3. Ricerca tutti i videogiochi");
+            Console.WriteLine("4. Cancella un videogioco");
+            Console.WriteLine("5. Chiudere il programma");
+
+            //Controllo che l'input inserito sia un numero intero
+            int choice;
+            while (!int.TryParse(Console.ReadLine(), out choice))
+            {
+                Console.WriteLine("Scelta non valida. Riprova.");
+                continue;
+            }
+
+            switch(choice)
+            {
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+                default:
+                    Console.WriteLine("Scelta non valida riprova!");
+                    break;
+            }
+
         }
     }
 }
